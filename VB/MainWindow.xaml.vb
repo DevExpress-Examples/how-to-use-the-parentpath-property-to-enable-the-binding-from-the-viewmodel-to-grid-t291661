3b3022ae78bd2d45c16699c9123cc9e6287b6bd3
DevExpressExampleBot <fakeEmail>
2022-12-17 14:47:29 +0400

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,17 +1,12 @@
-﻿Imports DevExpress.Xpf.Editors
-Imports DevExpress.Xpf.Grid
-Imports DevExpress.Xpf.Printing
-Imports System
-Imports System.Collections.Generic
 Imports System.Windows
-Imports System.Windows.Documents
 
 Namespace MultiLevelMasterDetail
-    Partial Public Class MainWindow
+
+    Public Partial Class MainWindow
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
             DataContext = New MultiLevelDataSource()
         End Sub
     End Class
