a27534dbe8e71e9358593ddf451778dbf15b1a27
AndreySlabov <andrey.slabov@devexpress.com>
2023-07-27 15:33:57 +0400

    Format Sample

---
+++ CS/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace MultiLevelMasterDetail {
+    public class MainViewModel : ViewModelBase {
+        public MainViewModel() {
+            Items = new ObservableCollection<MasterLevelItem>();
+            InitMasterItems();
+            CurrentMasterItem = null;
+        }
+        public ObservableCollection<MasterLevelItem> Items { get; private set; }
+        public MasterLevelItem CurrentMasterItem { get { return GetValue<MasterLevelItem>(); } set { SetValue(value); } }
+        public DetailLevelItem CurrentDetailItem { get { return GetValue<DetailLevelItem>(); } set { SetValue(value); } }
+
+        void InitMasterItems() {
+            for(int i = 0; i < 10; i++) {
+                Items.Add(new MasterLevelItem() {
+                    MasterId = i,
+                    MasterName = String.Format("master item {0}", i),
+                });
+            }
+        }
+    }
+
+    public class MasterLevelItem {
+        public int MasterId { get; set; }
+        public string MasterName { get; set; }
+        public List<DetailLevelItem> DetailItems { get; set; }
+        public MasterLevelItem() {
+            DetailItems = new List<DetailLevelItem>();
+            InitDetailItems();
+        }
+        void InitDetailItems() {
+            for(int i = 0; i < 10; i++) {
+                DetailItems.Add(new DetailLevelItem() {
+                    DetailId = i,
+                    DetailName = String.Format("detail item {0}", i),
+                    MasterItem = this,
+                });
+            }
+        }
+    }
+
+    public class DetailLevelItem {
+        public int DetailId { get; set; }
+        public string DetailName { get; set; }
+        public object MasterItem { get; set; }
+    }
+}
--- CS/MainWindow.xaml.cs
+++ CS/MainWindow.xaml.cs
@@ -1,16 +1,9 @@
-﻿using DevExpress.Xpf.Editors;
-using DevExpress.Xpf.Grid;
-using DevExpress.Xpf.Printing;
-using System;
-using System.Collections.Generic;
-using System.Windows;
-using System.Windows.Documents;
+﻿using System.Windows;
 
 namespace MultiLevelMasterDetail {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
-            DataContext = new MultiLevelDataSource();
         }
     }
 }
